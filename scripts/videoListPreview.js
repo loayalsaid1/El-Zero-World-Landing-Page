2348f1bcc6aeb8c0b321b0d9c77375c5f0df6748
Loay Al-Said <l.programming555@gmail.com>
2025-02-07 23:15:32 +0200

    Make a function to give hte clicked element active lcass

--- scripts/videoListPreview.js
+++ scripts/videoListPreview.js
@@ -4,16 +4,23 @@ const previewDiv = section.querySelector('.preview');
 const thumbnail = section.querySelector('.preview img');
 const nameParagraph = section.querySelector('.name');
 
+
+// Give the element the active class
+const setActiveVideoTitle = (videoElement) => {
+	videos.forEach(video => video.classList.remove('active'));
+	videoElement.classList.add('active');
+}
+
 // Set the previdw
 videos.forEach(titleElement => {
-	titleElement.addEventListener('click', () => {
+	titleElement.addEventListener('click', (e) => {
 		// Set the preview thumbnail
 		const imageName = titleElement.dataset.thumbnail;
 		thumbnail.src = `../images/${imageName}`;
 		// Set the preview title....
 		const title = titleElement.querySelector('.title').textContent;
 		nameParagraph.textContent = title;
+
+		setActiveVideoTitle(titleElement);
 	})
 })
-
-
