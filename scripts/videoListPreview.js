925b0a706034f3e2f8d34f9efaa789b4b7ce58b5
Loay Al-Said <l.programming555@gmail.com>
2025-02-07 23:07:28 +0200

    Give the clicked title an active class and style

--- scripts/videoListPreview.js
+++ scripts/videoListPreview.js
@@ -4,11 +4,16 @@ const previewDiv = section.querySelector('.preview');
 const thumbnail = section.querySelector('.preview img');
 const nameParagraph = section.querySelector('.name');
 
+// Set the previdw
 videos.forEach(titleElement => {
 	titleElement.addEventListener('click', () => {
+		// Set the preview thumbnail
 		const imageName = titleElement.dataset.thumbnail;
 		thumbnail.src = `../images/${imageName}`;
+		// Set the preview title....
 		const title = titleElement.querySelector('.title').textContent;
 		nameParagraph.textContent = title;
 	})
 })
+
+
